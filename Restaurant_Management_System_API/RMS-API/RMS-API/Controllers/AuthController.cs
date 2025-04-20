11d61a40aaf4f1d2a0b7635126aa4f054a3b877a
madaljeh <ahmadmadaljeh@gmail.com>
2025-04-21 02:26:49 +0300

    Reset Password API

---
+++ Restaurant_Management_System_API/RMS-API/RMS-API/Controllers/AuthController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using RMS_API.DTOs.AuthDTO.Request;
+namespace RMS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("Generate OTP")]
+
+        public async Task<IActionResult> GenerateOTP([FromBody] string Email)
+        {
+
+            try
+            {
+                string connectionString = "Data Source=LAPTOP-QGFR6N5D;Initial Catalog=RMS;Integrated Security=True;Trust Server Certificate=True";
+
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand("GenerateOTP", sqlConnection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Email", Email);
+
+                await sqlConnection.OpenAsync();
+                var resulte = await command.ExecuteNonQueryAsync();
+                
+                return Ok(new
+                {
+                    OTP = resulte,
+                });
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("RestPassword")]
+
+        public async Task<IActionResult> RestPassword([FromBody] RestPasswordDTO input)
+        {
+            try
+            {
+                string connectionString = "Data Source=LAPTOP-QGFR6N5D;Initial Catalog=RMS;Integrated Security=True;Trust Server Certificate=True";
+
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand("ResetPassword", sqlConnection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@Email",input.Email);
+                command.Parameters.AddWithValue("@OTPCode", input.OTPCode);
+                command.Parameters.AddWithValue("@NewPassword", input.NewPassword);
+
+                await sqlConnection.OpenAsync();
+                using var reader = await command.ExecuteReaderAsync();
+                
+                if (await reader.ReadAsync())
+                {
+                    return Ok("Update Successful");
+                }
+                
+                return StatusCode(200, "Update Successful");
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500,ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Restaurant_Management_System_API/RMS-API/RMS-API/DTOs/AuthDTO/Request/RestPasswordDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RMS_API.DTOs.AuthDTO.Request
+{
+    public class RestPasswordDTO
+    {
+       
+            public string Email { get; set; }
+            public int OTPCode { get; set; }
+            public string NewPassword { get; set; }
+        
+    }
+}
